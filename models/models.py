df0668d8abe1135df02d3970112d6bb1ada775ee
Nils Neeb <nils.neeb@students.unibe.ch>
2023-03-15 13:28:59 +0100

    Implemented Luc's duration function and added frequency field

--- models/models.py
+++ models/models.py
@@ -12,3 +12,15 @@ class meeting_scheduler(models.Model):
     end_date = fields.Datetime(string="End Date")
     location = fields.Char(string="Location")
     subject = fields.Text(string="Subject")
+    duration = fields.Char(string="Duration", compute="_calc_duration", store=True)
+    frequency = fields.Selection([('0', 'Not repeating'), ('1', 'Weekly'), ('2', 'Biweekly')], store=True,
+                                 string="Repeating", default='0')
+
+    @api.depends('start_date', 'end_date')
+    def _calc_duration(self):
+
+        try:
+            for record in self:
+                record.duration = record.end_date - record.start_date
+        except:
+            record.duration = "Error"
