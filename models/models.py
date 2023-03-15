31703147a79b8772087779aafeb4b5bac2cbad89
Sandro Calce <sandro.calce@students.unibe.ch>
2023-03-15 15:16:04 +0100

    Fixed faulty display and changed variable names to snake casing

--- models/models.py
+++ models/models.py
@@ -2,24 +2,25 @@
 
 from odoo import models, fields, api
 
+
 class meeting_scheduler(models.Model):
     _name = 'meeting_scheduler'
     _description = 'meeting_scheduler'
 
-    meetingTitle = fields.Char(string="Meeting Title")
-    meetingStartDate = fields.Datetime(string="Start Date")
-    meetingEndDate = fields.Datetime(string="End Date")
-    meetingLocation = fields.Char(string="Location")
-    meetingSubject = fields.Text(string="Subject")
-    meetingDuration = fields.Char(string="Duration", compute="_calc_duration", store=True)
-    meetingFrequency = fields.Selection([('0', 'Not repeating'), ('1', 'Weekly'), ('2', 'Biweekly')], store=True,
+    meeting_title = fields.Char(string="Meeting Title")
+    meeting_start_date = fields.Datetime(string="Start Date")
+    meeting_end_date = fields.Datetime(string="End Date")
+    meeting_location = fields.Char(string="Location")
+    meeting_subject = fields.Text(string="Subject")
+    meeting_duration = fields.Char(string="Duration", compute="_calc_duration", store=True)
+    meeting_frequency = fields.Selection([('0', 'Not repeating'), ('1', 'Weekly'), ('2', 'Biweekly')], store=True,
                                  string="Repeating", default='0')
 
-    @api.depends('meetingStartDate', 'meetingEndDate')
+    @api.depends('meeting_start_date', 'meeting_end_date')
     def _calc_duration(self):
 
         try:
             for record in self:
-                record.meetingDuration = record.meetingEndDate - record.meetingStartDate
+                record.meeting_duration = record.meeting_end_date - record.meeting_start_date
         except:
-            record.meetingDuration = "Error"
+            record.meeting_duration = "Error"
