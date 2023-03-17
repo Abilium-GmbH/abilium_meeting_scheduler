564865e2d9b45df7cf2a70219b0020b0c633053c
NNeeb <nils.neeb@students.unibe.ch>
2023-03-17 14:27:43 +0100

    create function creates multiple records

--- models/models.py
+++ models/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from datetime import timedelta
 
 from odoo import models, fields, api
 
@@ -14,14 +15,26 @@ class meeting_scheduler(models.Model):
     meeting_subject = fields.Text(string="Subject")
     meeting_duration = fields.Char(string="Duration", compute="_calc_duration", store=True)
     meeting_frequency = fields.Selection([('0', 'Not repeating'), ('1', 'Weekly'), ('2', 'Biweekly')], store=True,
-                                 string="Repeating", default='0')
+                                         string="Repeating", default='0')
+    meeting_repetitions = fields.Integer(string="Number of repetitions")
 
     @api.depends('meeting_start_date', 'meeting_end_date')
     def _calc_duration(self):
-
         try:
             for record in self:
                 record.meeting_duration = record.meeting_end_date - record.meeting_start_date
         except:
             record.meeting_duration = "Error"
 
+    @api.model_create_multi
+    def create(self, data_list):
+
+        created_records = []
+
+        data_list[0]['meeting_title'] = "stage 1"
+
+        for x in range(5):
+            data_list[0]['meeting_subject'] = "rec xx " + str(x)
+            created_records.append(super(meeting_scheduler, self).create(data_list))
+
+        return created_records[0]
