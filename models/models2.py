daaf4b75d82d5a985d892125ac764daf4bf0951f
LV <100426199+Lvk-v@users.noreply.github.com>
2023-03-24 13:03:39 +0100

    Add files via upload
    added 2 buttons in group_scheduler
    defined function button_function_test where the comments describe what to implement

--- models/models2.py
+++ models/models2.py
@@ -10,3 +10,14 @@ class group_scheduler(models.Model):
 
     meeting_group = fields.Char(string="Meeting Group", required=True)
     meeting_attendees = fields.Many2many('res.users', string="Attendees")
+
+    def button_function_test(self):
+        grouplist = self
+        # grouplist.meeting_attendees.
+# get ids from group members
+# partner_id NOT EQUAL to user_id!!
+# group_scheduler_res_users_rel has user ids and group ids
+# get partner_id from res_users
+# get related calendar_event_id from  calendar_event_res_partner_rel
+# get related meetings from calendar_event
+# calc freetime with stardate enddate
