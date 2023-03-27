d6073c6f4635c5c360a3a9b064b6cc3d012d66a4
LV <100426199+Lvk-v@users.noreply.github.com>
2023-03-27 17:13:55 +0200

    Add files via upload
    added function to get events from calendar according to selected groups, 
    also eliminates duplicate records in the search results

--- models/models2.py
+++ models/models2.py
@@ -9,7 +9,11 @@ class group_scheduler(models.Model):
     _description = 'group_scheduler'
 
     meeting_group = fields.Char(string="Meeting Group", required=True)
-    meeting_attendees = fields.Many2many('res.users', string="Attendees")
+    meeting_attendees = fields.Many2many('res.users', string="Attendees") #todo partners
+
+    # those variables need to apear in a search form
+    # meeting_search_start_date = fields.Datetime(string="Search Start Date", required=True)
+    # meeting_search_end_date = fields.Datetime(string="Search End Date", required=True)
 
     def button_function_test(self):
         group_selected_ids = self.env.context.get('active_ids', [])
@@ -30,15 +34,22 @@ class group_scheduler(models.Model):
         for partners in partner_id_records:
             for partner_id_entry in partners['partner_id']:
                 partner_id_list.append(partner_id_entry['id'])
-        self.env['print_table'].create({'show_stuff': partner_id_list})
+        # self.env['print_table'].create({'show_stuff': partner_id_list})
 
     # get related calendar_event_id from  calendar_event_res_partner_rel
-    #     meeting_found = self.env['calendar.event.res.partner.rel'].search([('res_partner_id', '=', 2)])
-    #     for meeting_entry in meeting_found:
-    #         self.env['print_table'].create({'show_stuff': meeting_entry})
-
     # get related meetings from calendar_event
-    # calc freetime with stardate enddate
-
-
-
+        meeting_found = self.env['calendar.event'].search([])
+        meeting_selected_list = []
+        for ting in meeting_found:
+            for uid in partner_id_list: #group_res_users_all_ids:
+                for pflopf in ting.attendee_ids.partner_id:
+                    if(uid == pflopf.id):
+                        meeting_selected_list.append(ting)
+                        # self.env['print_table'].create({'show_stuff': str(ting.name) + ', ' + str(ting.start) + ', ' + str(ting.attendee_ids.partner_id)})
+        meeting_selected_list = list(dict.fromkeys(meeting_selected_list))
+        for x in meeting_selected_list:
+            self.env['print_table'].create(
+                {'show_stuff': str(x.name) + ', '
+                               + str(x.start) + ', '
+                               + str(x.stop) + ', '
+                               + str(x.attendee_ids.partner_id)})
