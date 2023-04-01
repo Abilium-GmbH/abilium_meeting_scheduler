f69d513f2517a9f057631a0b9f5398df278db6ff
LV <100426199+Lvk-v@users.noreply.github.com>
2023-04-01 16:53:37 +0200

    Add files via upload
    added views from valerio
    template test unsuccessfull, changes in view and controller

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from odoo import http
-
+from odoo.http import route, request
 
 class MeetingScheduler(http.Controller):
     @http.route('/meeting_scheduler/meeting_scheduler/', auth='public')
     def index(self, **kw):
         return "Hello, world"
+
+    @route('/meeting_scheduler/guest_view/', auth='public', website=True)
+    def index(self, **kw):
+        records = request.env['meeting_scheduler'].sudo().search([])
+        response = request.render("meeting_scheduler.jkdfjk")
+        return response
+
 
     @http.route('/meeting_scheduler/meeting_scheduler/objects/', auth='public')
     def list(self, **kw):
