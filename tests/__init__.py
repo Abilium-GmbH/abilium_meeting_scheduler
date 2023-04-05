b53c1e0ff6d43bb7c774354fa2f37af24dd5925f
Nils Neeb <nils.neeb@students.unibe.ch>
2023-04-05 19:04:27 +0200

    created test for repetition numbers and updated init

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,3 +2,4 @@ from . import test_dummy_test
 from . import test_meeting_duration
 from . import test_calendar_event_creation
 from . import test_creating_repetitions
+from . import test_repetition_number
