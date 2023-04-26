b71518fb0911ac97f91061131f1da6447161fc58
Tim Kuhn <93331235+TJKuhn@users.noreply.github.com>
2023-04-26 15:52:30 +0200

    Added test_overwrite import

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,3 +3,4 @@ from . import test_meeting_duration
 from . import test_calendar_event_creation
 from . import test_creating_repetitions
 from . import test_repetition_number
+from . import test_overwrite
